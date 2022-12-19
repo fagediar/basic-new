e99cef6c04200f1633e239b840a7d92d4b446869
fausto diaz <fausto@aplicacionescorporativas.com>
2022-12-19 17:30:51 -0500

    corregir

---
+++ app/post.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App;
+
+use Cviebrock\EloquentSluggable\Sluggable;
+use Illuminate\Database\Eloquent\Model;
+
+class post extends Model
+{
+    use Sluggable;
+
+    
+    /**
+     * Return the sluggable configuration array for this model.
+     *
+     * @return array
+     */
+    public function sluggable(): array
+    {
+        return [
+            'slug' => [
+                'source' => 'title',
+                'onUpdate' => true
+            ]
+        ];
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
+
+
+
